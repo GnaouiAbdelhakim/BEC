2d12073d6a280f3b726deda1703ded7b52d9dfe4
Gnaoui Dev <gnaoui.abdelhakim@gmail.com>
2015-09-03 14:54:56 +1000

    creation nouvelle branche

--- extra-addons2/bec_purchase/purchase.py
+++ extra-addons2/bec_purchase/purchase.py
@@ -22,6 +22,7 @@
 
 from datetime import datetime
 import time
+import times
 from openerp.osv import fields, osv
 from openerp.tools.translate import _
 from openerp.tools import DEFAULT_SERVER_DATE_FORMAT, DEFAULT_SERVER_DATETIME_FORMAT
